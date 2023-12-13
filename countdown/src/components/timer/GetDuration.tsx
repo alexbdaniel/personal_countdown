fbcff54ca7bfce4ea481e7b24f0c9dc9b45fc06f
= <=>
2023-12-14 00:17:34 +0800

    removed unused declarations

--- countdown/src/components/timer/GetDuration.tsx
+++ countdown/src/components/timer/GetDuration.tsx
@@ -2,10 +2,9 @@ import { DateTime as LuxonDateTime, DurationObjectUnits } from 'luxon'
 
 
 
-export const GetDuration = (target: LuxonDateTime, offsetHours: number = 0, offsetMinutes: number = 0): DurationObjectUnits => {
+export const GetDuration = (target: LuxonDateTime): DurationObjectUnits => {
   const now: LuxonDateTime = LuxonDateTime.now()
-  offsetHours = 0;
-  offsetMinutes = 0;
+
 
   let diff: DurationObjectUnits = target.diff(now, ['days', 'hours', 'minutes', 'seconds']).toObject()
 
